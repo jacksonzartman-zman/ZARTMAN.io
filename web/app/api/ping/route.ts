d4606a034a5808909217fcfbbe4272f141bf8085
jacksonzartman-zman <jackson.zartman@gmail.com>
2025-11-13 19:47:16 +0000

    chore(api): add cache-control no-store to ping route

--- web/app/api/ping/route.ts
+++ web/app/api/ping/route.ts
@@ -3,12 +3,20 @@ export const runtime = 'edge';
 
 export async function GET() {
 	return new Response('pong:GET', {
-		headers: { 'content-type': 'text/plain; charset=utf-8', 'x-served-by': 'next-app-edge' },
+		headers: {
+			'content-type': 'text/plain; charset=utf-8',
+			'cache-control': 'no-store',
+			'x-served-by': 'next-app-edge',
+		},
 	})
 }
 
 export async function POST() {
 	return new Response('pong:POST', {
-		headers: { 'content-type': 'text/plain; charset=utf-8', 'x-served-by': 'next-app-edge' },
+		headers: {
+			'content-type': 'text/plain; charset=utf-8',
+			'cache-control': 'no-store',
+			'x-served-by': 'next-app-edge',
+		},
 	})
 }
